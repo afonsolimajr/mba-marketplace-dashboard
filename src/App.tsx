ac58084f252a5b04de17ac6870302f619e66c9d4
Afonso Lima <afonsolima@gmail.com>
2025-03-23 18:26:46 -0300

    adding tailwind and shadCN to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import "@/index.css";
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="text-3xl">Pizza Shop</p>
+      <Button>Teste</Button>
+    </div>
+  );
 }
