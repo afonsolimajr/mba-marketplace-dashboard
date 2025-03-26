c52d250e164a5b89e270a1e1c374ac4325f7747a
Afonso Lima <afonsolima@gmail.com>
2025-03-26 14:30:10 -0300

    dashboard revenue chart layout

--- src/pages/_layouts/app-layout.tsx
+++ src/pages/_layouts/app-layout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
-      <div className="flex flex-1 flex-col gap-4 pt-6">
+      <div className="flex flex-1 flex-col gap-4 pt-6 px-6">
         <Outlet />
       </div>
     </div>
--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -2,6 +2,7 @@ import CanceledMonthOrdersAmountCard from "./canceled-month-orders-amount-card";
 import DayOrdersAmountCard from "./day-orders-amount-card";
 import MonthOrdersAmountCard from "./month-orders-amount-card";
 import MonthRevenueCard from "./month-revenue-card";
+import RevenueChart from "./revenue-chart";
 
 export default function Dashboard() {
   return (
@@ -12,6 +13,10 @@ export default function Dashboard() {
         <MonthOrdersAmountCard />
         <DayOrdersAmountCard />
         <CanceledMonthOrdersAmountCard />
+      </div>
+
+      <div className="grid grid-cols-9 gap-4">
+        <RevenueChart />
       </div>
     </div>
   );
