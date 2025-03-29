3ec2a6c6b1e9f5d651cb332426a133a9bdad4f3a
Afonso Lima <afonsolima@gmail.com>
2025-03-29 17:38:23 -0300

    axios setup and react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { App } from "./App";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
-      <BrowserRouter>
-        <Toaster richColors />
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Toaster richColors />
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 );
