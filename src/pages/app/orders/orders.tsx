e08f2402f8358241070fe9e7c43d634c28cd19d7
Afonso Lima <afonsolima@gmail.com>
2025-04-06 22:36:19 -0300

    pagination component

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -2,36 +2,38 @@ import { useQuery } from "@tanstack/react-query";
 import OrderFilter from "./OrderFilter";
 import OrderListItem from "./OrderListItem";
 import { getOrders } from "@/api/get-orders";
+import Pagination from "@/components/pagination";
 
 export default function Orders() {
   const { data: result } = useQuery({
     queryKey: ["orders"],
-    queryFn: getOrders,
+    queryFn: () => getOrders({ pageIndex: 1 }),
   });
 
   return (
     <div>
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderFilter />
-        <div className="border-b-2 items-center justify-center">
-          <div className="flex items-center gap-2">
-            <span className="w-16"></span>
-            <span className="w-36">Identificador</span>
-            <span className="w-40">realizado há</span>
-            <span className="w-36">Status</span>
-            <span className="w-80">Cliente</span>
-            <span className="w-44">Total do pedido</span>
-            <span className="w-24"></span>
-            <span className="w-24"></span>
+        <div className="space-y-2.5">
+          <OrderFilter />
+          <div className="flex flex-col border-t-2 items-center justify-center gap-1">
+            <div className="flex items-center gap-2">
+              <span className="w-16"></span>
+              <span className="w-36">Identificador</span>
+              <span className="w-40">realizado há</span>
+              <span className="w-36">Status</span>
+              <span className="w-80">Cliente</span>
+              <span className="w-44">Total do pedido</span>
+              <span className="w-24"></span>
+              <span className="w-24"></span>
+            </div>
+            {result &&
+              result.orders.map((order, index) => {
+                return <OrderListItem key={index} order={order} />;
+              })}
           </div>
+          <Pagination pageIndex={0} perPage={10} totalCount={56} />
         </div>
-        {result &&
-          result.orders.map((order, index) => {
-            return <OrderListItem key={index} order={order} />;
-          })}
       </div>
     </div>
   );
